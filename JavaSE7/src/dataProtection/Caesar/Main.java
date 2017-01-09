45d5006897ebb591333fc3212bcb7c472f50b4d8
carbonqwe123 <carbonqwe123@users.noreply.github.com>
2017-01-09 16:41:36 +0300

    Update Main.java

--- JavaSE7/src/dataProtection/Caesar/Main.java
+++ JavaSE7/src/dataProtection/Caesar/Main.java
@@ -1,4 +1,14 @@
 package dataProtection.Caesar;
 
 public class Main {
+  public static void main(String[] args) {
+        System.out.print("Enter the number: ");
+        Scanner scanInput = new Scanner(System.in);
+        if (scanInput.hasNextInt()) {
+            int number = scanInput.nextInt();
+            System.out.print("Вы ввели число: ");
+            System.out.println(number);
+        }
+
+    }
 }
